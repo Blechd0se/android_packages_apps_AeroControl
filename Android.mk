33dbd6307bca28f0c112e5a5966a319fb337db70
Jesus David <jesdga95@gmail.com>
2013-07-20 14:04:23 -0500

    Custom android-support-v4 prebuilt/ static name
    Fixes compilation crash
    
    Change-Id: Iba4561cb60ff06461d8d41c6083dddd95dd6b6d0

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES += $(call all-java-files-under, src)
  
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4
+    android-support-v4_13
  
 LOCAL_PACKAGE_NAME := ParanoidOTA
 LOCAL_CERTIFICATE := platform
@@ -15,6 +15,6 @@ include $(BUILD_PACKAGE)
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    android-support-v4:/libs/android-support-v4.jar
+    android-support-v4_13:/libs/android-support-v4.jar
 
 include $(BUILD_MULTI_PREBUILT)
