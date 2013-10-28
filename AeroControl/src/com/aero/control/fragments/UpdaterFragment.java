3cbfda4808d44f3fbdfdfc956ff4d7edf08d42fc
Alexander Christ <alex.christ@hotmail.de>
2013-10-28 17:20:10 +0100

    updater: add restore option
    To restore a backup. Deletes the old zImage first.

--- AeroControl/src/com/aero/control/fragments/UpdaterFragment.java
+++ AeroControl/src/com/aero/control/fragments/UpdaterFragment.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
@@ -58,14 +59,65 @@ public class UpdaterFragment extends PreferenceFragment {
 
         final Preference backup_kernel = root.findPreference("backup_kernel");
         final Preference updater_kernel = root.findPreference("updater_kernel");
-        final Preference restore_kernel = root.findPreference("restore_kernel");
+        final ListPreference restore_kernel = (ListPreference) root.findPreference("restore_kernel");
 
         // Disable them all;
         //backup_kernel.setEnabled(false);
         updater_kernel.setEnabled(false);
-        restore_kernel.setEnabled(false);
+        //restore_kernel.setEnabled(false);
 
-        backup_kernel.setSummary("Last Backup from: " + shell.getDirInfo("/sdcard/com.aero.control/", false)[0]);
+        backup_kernel.setSummary(getText(R.string.last_backup_from)+ " " + shell.getDirInfo("/sdcard/com.aero.control/", false)[0]);
+
+        restore_kernel.setEntries(shell.getDirInfo("/sdcard/com.aero.control/", false));
+        restore_kernel.setEntryValues(shell.getDirInfo("/sdcard/com.aero.control/", false));
+
+        restore_kernel.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object o) {
+
+                final String s = (String) o;
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                View layout = inflater.inflate(R.layout.about_screen, null);
+                TextView aboutText = (TextView) layout.findViewById(R.id.aboutScreen);
+
+                builder.setTitle(getText(R.string.backup_from) + " " + s);
+
+                aboutText.setText(getText(R.string.restore_from_backup) + " " + s + " ?");
+                shell.remountSystem();
+
+                builder.setView(layout)
+                        .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                // Delete old zImage first, then copy backup;
+                                String[] commands = new String[]
+                                        {
+                                                "rm -f " + "/system/bootmenu/2nd-boot/zImage",
+                                                "cp " + "/sdcard/com.aero.control/" + s + "/zImage" + " /system/bootmenu/2nd-boot/zImage",
+                                        };
+
+                                shell.setRootInfo(commands);
+
+                                Toast.makeText(getActivity(), R.string.need_reboot, Toast.LENGTH_LONG).show();
+
+
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                // Do nothing
+                            }
+                        });
+
+                builder.show();
+
+                return true;
+            };
+        });
 
         backup_kernel.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
@@ -78,7 +130,7 @@ public class UpdaterFragment extends PreferenceFragment {
 
                 builder.setTitle("Backup");
 
-                aboutText.setText("Proceed with backup? This will backup your current kernel.");
+                aboutText.setText(R.string.proceed_backup);
 
                 builder.setView(layout)
                         .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
@@ -86,10 +138,10 @@ public class UpdaterFragment extends PreferenceFragment {
                             public void onClick(DialogInterface dialog, int id) {
 
                                 try {
-                                    update.copyFile(IMAGE, BACKUP_PATH , timeStamp);
+                                    update.copyFile(IMAGE, BACKUP_PATH, false);
                                     Toast.makeText(getActivity(), "Backup was successful!", Toast.LENGTH_LONG).show();
 
-                                    backup_kernel.setSummary("Last Backup from: " + timeStamp);
+                                    backup_kernel.setSummary(R.string.last_backup_from + timeStamp);
 
                                 } catch (IOException e) {
                                     Log.e("Aero", "A problem occured while saving a backup", e);
@@ -99,7 +151,7 @@ public class UpdaterFragment extends PreferenceFragment {
                         .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
 
-                                    // Do nothing
+                                // Do nothing
                             }
                         });
 
--- AeroControl/src/com/aero/control/helpers/updateHelpers.java
+++ AeroControl/src/com/aero/control/helpers/updateHelpers.java
@@ -8,6 +8,8 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 /**
@@ -16,37 +18,47 @@ import java.nio.channels.FileChannel;
  */
 public class updateHelpers {
 
+    public static String timeStamp = new SimpleDateFormat("ddMMyyyy").format(Calendar.getInstance().getTime());
+
     /**
      * Method for copying files.
      *
      * @param original  => The original file (+Path)
      * @param copy      => The new file      (+Path)
-     * @param timeStamp => (TimeStamp) subfolder of com.aero.control
      */
-    public void copyFile(File original, File copy, String timeStamp) throws IOException {
+    public void copyFile(File original, File copy, boolean rest) throws IOException {
 
         FileChannel input = null;
         FileChannel output = null;
 
-        try {
-            if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-                Log.e("Aero", "No Sdcard found!");
-                return;
+        if (!rest) {
+            try {
+                if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+                    Log.e("Aero", "No Sdcard found!");
+                    return;
 
-            } else {
-                File file = new File(Environment.getExternalStorageDirectory()
-                        +File.separator
-                        +"com.aero.control" //folder name
-                        +File.separator
-                        +timeStamp); //file name
-                file.mkdirs();
+                } else {
+                    File file = new File(Environment.getExternalStorageDirectory()
+                            +File.separator
+                            +"com.aero.control" //folder name
+                            +File.separator
+                            +timeStamp); //file name
+                    file.mkdirs();
+                }
+
+                input = new FileInputStream(original).getChannel();
+                output = new FileOutputStream(copy).getChannel();
+                output.transferFrom(input, 0, input.size());
+
+            } finally {
+                input.close();
+                output.close();
             }
-
+        } else {
             input = new FileInputStream(original).getChannel();
             output = new FileOutputStream(copy).getChannel();
             output.transferFrom(input, 0, input.size());
 
-        } finally {
             input.close();
             output.close();
         }
