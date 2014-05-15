6e137d0333ceff93894e2cd0f6485591da6fdc09
Blechd0se <alex.christ@hotmail.de>
2014-05-15 20:39:35 +0200

    helpers: allow to change the font for each prefence
    I am not sure yet how i will use this properly, but giving
    an option to change the font on the fly is really nice.
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- AeroControl/src/com/aero/control/helpers/CustomTextPreference.java
+++ AeroControl/src/com/aero/control/helpers/CustomTextPreference.java
@@ -1,8 +1,12 @@
 package com.aero.control.helpers;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.preference.EditTextPreference;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Alexander Christ on 30.09.13.
@@ -10,6 +14,10 @@ import android.util.AttributeSet;
 public class CustomTextPreference extends EditTextPreference {
 
     private Context context;
+    private int style;
+    public static final Typeface kitkatFont = Typeface.create("sans-serif-condensed", Typeface.NORMAL);
+    public static final int STYLE_KITKAT = 10;
+    public static final int STYLE_NORMAL = 20;
 
     public CustomTextPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -33,5 +41,48 @@ public class CustomTextPreference extends EditTextPreference {
 	public void setContext(Context context) {
 		this.context = context;
 	}
+
+    private void setStyleKitkat(View view) {
+        TextView titleView = (TextView) view.findViewById(android.R.id.title);
+        TextView summaryView = (TextView) view.findViewById(android.R.id.summary);
+        titleView.setTypeface(kitkatFont);
+        summaryView.setTypeface(kitkatFont);
+    }
+
+    private void setStyleNormal(View view) {
+        TextView titleView = (TextView) view.findViewById(android.R.id.title);
+        TextView summaryView = (TextView) view.findViewById(android.R.id.summary);
+        titleView.setTypeface(Typeface.DEFAULT);
+        summaryView.setTypeface(Typeface.DEFAULT);
+    }
+
+        @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        switch (style) {
+            case STYLE_KITKAT:
+                setStyleKitkat(view);
+                break;
+            case STYLE_NORMAL:
+                setStyleNormal(view);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void setStyle(int style) {
+        switch (style) {
+            case STYLE_KITKAT:
+                this.style = STYLE_KITKAT;
+                break;
+            case STYLE_NORMAL:
+                this.style = STYLE_NORMAL;
+                break;
+            default:
+                this.style = STYLE_NORMAL;
+        }
+    }
 
 }
