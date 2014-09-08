9e0804b9df1e6f5eda920e258aeeca88bab15f61
Blechd0se <alex.christ@hotmail.de>
2014-09-09 00:48:51 +0200

    preferences: allow to disable the testsuite
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- AeroControl/src/com/aero/control/prefs/PrefsActivity.java
+++ AeroControl/src/com/aero/control/prefs/PrefsActivity.java
@@ -2,6 +2,7 @@ package com.aero.control.prefs;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -26,6 +27,7 @@ import com.aero.control.AeroActivity;
 import com.aero.control.R;
 import com.aero.control.service.PerAppServiceHelper;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -253,6 +255,16 @@ public class PrefsActivity extends PreferenceActivity {
 
                 // Show testsuite if clicked;
                 if (mCounter >= 7) {
+
+                    ContextWrapper cw = new ContextWrapper(getBaseContext());
+                    File testsuite = new File(cw.getFilesDir() + "/testsuite");
+
+                    if (testsuite.exists()) {
+                        testsuite.delete();
+                        Toast.makeText(getApplicationContext(), "You have disabled the TestSuite!", Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+
                     try {
                         FileOutputStream fos = getApplicationContext().openFileOutput("testsuite", Context.MODE_PRIVATE);
                         fos.write("1".getBytes());
