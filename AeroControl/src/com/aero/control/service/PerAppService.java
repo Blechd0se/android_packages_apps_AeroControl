1e040deaf92ecea93f9419e61695ae5311c3edfd
Blechd0se <alex.christ@hotmail.de>
2014-09-29 22:31:38 +0200

    service: prepare perapp for android L
    At and above android L getRunningTasks() will be
    deprecated due to androids new document focused UI.
    Since it will only return some sort of pseudo-data
    per app profiles become useless.
    
    But we are smarter than google and check for all apps
    which are flagged with the IMPORTANCE_FOREGROUND flag.
    Since the actual visible app is the first one in that
    list, take it.
    
    Since L is not officially released, we might want to
    check this later again.
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- AeroControl/src/com/aero/control/service/PerAppService.java
+++ AeroControl/src/com/aero/control/service/PerAppService.java
@@ -5,6 +5,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Handler;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 
 import com.aero.control.helpers.settingsHelper;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -108,18 +110,35 @@ public final class PerAppService extends Service {
             }
         }
     }
-    private final void setAppData() {
+    private void setAppData() {
 
         String PackageName = mCurrentApp;
         mPreviousApp = PackageName;
 
         if (mAm == null)
             mAm = (ActivityManager) PerAppService.this.getSystemService(ACTIVITY_SERVICE);
-        // Get the first item in the list;
-        final ActivityManager.RunningTaskInfo AppInfo = mAm.getRunningTasks(1).get(0);
 
-        PackageName = AppInfo.topActivity.getPackageName();
+        // Get the first item in the list;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            PackageName = getTopApp();
+        } else {
+            ActivityManager.RunningTaskInfo AppInfo = mAm.getRunningTasks(1).get(0);
+
+            PackageName = AppInfo.topActivity.getPackageName();
+        }
 
         mCurrentApp = PackageName;
+    }
+
+    private String getTopApp() {
+
+        List<ActivityManager.RunningAppProcessInfo> appProcesses = mAm.getRunningAppProcesses();
+        for(ActivityManager.RunningAppProcessInfo appProcess : appProcesses){
+            if(appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND){
+                // Always return the first one, since this is the one that we are looking for;
+                return appProcess.processName;
+            }
+        }
+        return null;
     }
 }
