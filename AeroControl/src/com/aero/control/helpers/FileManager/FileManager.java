Blechd0se/android_packages_apps_AeroControl

commit e913c01bb854d407a9ba2a642ea09f9023183d2c

filemanager: small performance improvements
Don't re-init the whole adapter and the data
structure on each click, instead use the build-in
clear() method to optimize usage a bit.

Signed-off-by: Blechd0se <alex.christ@hotmail.de>

AeroControl/src/com/aero/control/helpers/FileManager/FileManager.java
@@ -7,8 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.aero.control.AeroActivity;
 import com.aero.control.R;
@@ -24,12 +24,14 @@ import java.util.List;
 /**
  * Created by Alexander Christ on 21.09.14.
  */
-public class FileManager extends LinearLayout implements OnItemClickListener {
+public class FileManager extends RelativeLayout implements OnItemClickListener {
 
     Context mContext;
     FileManagerListener mFolderListener;
     private static final String mRoot = "/";
     private ListView mListView;
+    private final static List<AeroData> mData = new ArrayList<AeroData>();
+    private FileAdapter mAdapter;
     private String mCurrentPath;
     private Dialog mDialog;
     private List<FileData> mFileData = null;
@@ -148,7 +150,7 @@ public class FileManager extends LinearLayout implements OnItemClickListener {
     }
 
     public void setItemList(List<FileData> item){
-        List<AeroData> mOverviewData= new ArrayList<AeroData>();
+        mData.clear();
 
         File checkFile;
         for (FileData a : item) {
@@ -157,16 +159,19 @@ public class FileManager extends LinearLayout implements OnItemClickListener {
             checkFile = new File(mCurrentPath + "/" + a.item);
 
             if (checkFile.isDirectory())
-                mOverviewData.add(new AeroData(R.drawable.file_folder, a.item));
+                mData.add(new AeroData(R.drawable.file_folder, a.item));
             else
-                mOverviewData.add(new AeroData(R.drawable.file_document, a.item));
+                mData.add(new AeroData(R.drawable.file_document, a.item));
         }
 
-        FileAdapter adapter = new FileAdapter(mContext,
-                R.layout.file_row, mOverviewData);
+        if (mAdapter == null) {
+            mAdapter = new FileAdapter(mContext, R.layout.file_row, mData);
 
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(this);
+            mListView.setAdapter(mAdapter);
+            mListView.setOnItemClickListener(this);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
 

AeroControl/res/layout/file_row.xml
@@ -1,21 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
-<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
     android:orientation="horizontal"
     android:layout_width="match_parent"
     android:layout_height="match_parent"
     android:padding="5dp">
 
+        <ImageView xmlns:android="http://schemas.android.com/apk/res/android"
+            android:id="@+id/rowfolder"
+            android:layout_width="wrap_content"
+            android:layout_height="match_parent"
+            android:padding="2dp"
+            android:layout_alignParentLeft="true" />
 
-    <ImageView xmlns:android="http://schemas.android.com/apk/res/android"
-        android:id="@+id/rowfolder"
-        android:layout_width="wrap_content"
-        android:layout_height="match_parent"
-        android:padding="2dp"/>
-
-    <TextView xmlns:android="http://schemas.android.com/apk/res/android"
-        android:id="@+id/rowtext"
-        android:layout_width="fill_parent"
-        android:textSize="18sp"
-        android:padding="4dp"
-        android:layout_height="match_parent"/>
-</LinearLayout>
+        <TextView xmlns:android="http://schemas.android.com/apk/res/android"
+            android:id="@+id/rowtext"
+            android:layout_width="wrap_content"
+            android:textSize="18sp"
+            android:padding="4dp"
+            android:layout_height="match_parent"
+            android:layout_toRightOf="@+id/rowfolder"/>
+</RelativeLayout>

AeroControl/src/com/aero/control/adapter/FileAdapter.java
@@ -73,8 +73,7 @@ public class FileAdapter extends ArrayAdapter<AeroData> {
 
             holder.header.setImageResource(overview.file);
 
-            if (!overview.content.equals("A"))
-                holder.content.setText(overview.content);
+            holder.content.setText(overview.content);
 
         } else {
             Log.e("Aero",

commit 01751a1c65774a3b3aa7d3fffc714664c7f2ecfd

filemanager: fix IndexOutOfBoundsException during onClick()
Commit e913c01bb854d407a9ba2a642ea09f9023183d2c introduced a nasty bug, which could crash
Aero Control during onClick() in the 'Your Settings' dialog.

Signed-off-by: Blechd0se <alex.christ@hotmail.de>

AeroControl/src/com/aero/control/helpers/FileManager/FileManager.java
@@ -30,7 +30,7 @@ public class FileManager extends RelativeLayout implements OnItemClickListener {
     FileManagerListener mFolderListener;
     private static final String mRoot = "/";
     private ListView mListView;
-    private final static List<AeroData> mData = new ArrayList<AeroData>();
+    private List<AeroData> mData = new ArrayList<AeroData>();
     private FileAdapter mAdapter;
     private String mCurrentPath;
     private Dialog mDialog;
@@ -145,15 +145,17 @@ public class FileManager extends RelativeLayout implements OnItemClickListener {
         mFileData.addAll(MetaDirectory);
         mFileData.addAll(MetaFiles);
 
-        setItemList(mFileData);
+        setItemList();
 
     }
 
-    public void setItemList(List<FileData> item){
-        mData.clear();
+    public void setItemList(){
+
+        if (mData != null)
+            mData.clear();
 
         File checkFile;
-        for (FileData a : item) {
+        for (FileData a : mFileData) {
 
             // Get the full path;
             checkFile = new File(mCurrentPath + "/" + a.item);