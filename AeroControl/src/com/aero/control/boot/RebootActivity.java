185553c8cd4f1731dae8aff0e0dd9d6cf6c11134
Blechd0se <alex.christ@hotmail.de>
2015-06-13 18:29:00 +0200

    boot: allow to disable reboot notifier on reboot
    Give the user a quick chance to disable this feature,
    if desired.
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- AeroControl/src/com/aero/control/boot/RebootActivity.java
+++ AeroControl/src/com/aero/control/boot/RebootActivity.java
@@ -3,9 +3,12 @@ package com.aero.control.boot;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.aero.control.R;
@@ -24,8 +27,11 @@ public class RebootActivity extends Activity {
             LayoutInflater inflater = this.getLayoutInflater();
 
             // Just reuse aboutScreen, because its Linear and has a TextView
-            View layout = inflater.inflate(R.layout.about_screen, null);
+            View layout = inflater.inflate(R.layout.reboot_screen, null);
             TextView aboutText = (TextView) layout.findViewById(R.id.aboutScreen);
+            final CheckBox checkBox = (CheckBox) layout.findViewById(R.id.reboot_checkbox);
+
+            checkBox.setText(R.string.dont_show_again);
 
             builder.setCancelable(false);
             builder.setIcon(R.drawable.warning);
@@ -37,6 +43,13 @@ public class RebootActivity extends Activity {
                     .setPositiveButton(R.string.got_it, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             dialog.cancel();
+
+                            // if it is checked, dont display it again;
+                            if (checkBox.isChecked()) {
+                                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                                pref.edit().putBoolean("reboot_checker", false).apply();
+                            }
+
                             finish();
                         }
                     });
