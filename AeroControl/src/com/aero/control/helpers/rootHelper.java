df30e946fdb29e116e43bea6ae4cd44a5144d611
Blechd0se <alex.christ@hotmail.de>
2015-11-01 17:40:14 +0100

    helper: rootHelper: check for "id" too
    This file wasn't touched in ages, it only checked for the existence
    of the su-binary or a superuser.apk. Since this is not really a good
    practice, we switch to a call for "id" to check, if we are actually
    root.
    
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- AeroControl/src/com/aero/control/helpers/rootHelper.java
+++ AeroControl/src/com/aero/control/helpers/rootHelper.java
@@ -2,7 +2,10 @@ package com.aero.control.helpers;
 
 import android.util.Log;
 
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Alexander Christ on 26.12.13.
@@ -11,12 +14,15 @@ import java.io.File;
 public class rootHelper {
 
     private static final String LOG_TAG = rootHelper.class.getName();
+    private final static String NO_DATA_FOUND = "Unavailable";
+    private static final int BUFF_LEN = 1024;
+    private static final byte[] buffer = new byte[BUFF_LEN];
 
         public boolean isDeviceRooted() {
-            return checkRootMethod1() || checkRootMethod2();
+            return checkRootMethod1() || checkRootMethod2() || checkRootMethod3();
         }
 
-        public boolean checkRootMethod1() {
+        private boolean checkRootMethod1() {
             try {
                 File file = new File("/system/app/Superuser.apk");
                 return file.exists();
@@ -27,7 +33,7 @@ public class rootHelper {
 
         }
 
-        public boolean checkRootMethod2() {
+        private boolean checkRootMethod2() {
             try {
                 File file = new File("/system/xbin/su");
                 return file.exists();
@@ -35,6 +41,58 @@ public class rootHelper {
                 Log.e(LOG_TAG, "An Error occured while checking for su.", e);
                 return false;
             }
+        }
+
+        private boolean checkRootMethod3() {
+
+            String output = suCheckRootMethod();
+
+            if (output.equals(NO_DATA_FOUND)) {
+                return false;
+            }
+
+            // Return true if we are rooted;
+            return output.contains("uid=0");
+
+        }
+
+        private String suCheckRootMethod() {
+            Process rooting = null;
+            try {
+                rooting = Runtime.getRuntime().exec("su");
+
+                DataOutputStream stdin = new DataOutputStream(rooting.getOutputStream());
+
+                stdin.writeBytes("id" + "\n");
+                InputStream stdout = rooting.getInputStream();
+                int read;
+                String output = "";
+                while(true){
+                    read = stdout.read(buffer);
+                    if (read == -1)
+                        return NO_DATA_FOUND;
+
+                    output += new String(buffer, 0, read);
+                    if(read<BUFF_LEN){
+                        //we have read everything
+                        break;
+                    }
+                }
+                stdin.writeBytes("exit\n");
+                return output;
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Do you even root, bro? :/", e);
+            } finally {
+                if (rooting != null) {
+                    try {
+                        rooting.waitFor();
+                    } catch (InterruptedException e) {}
+                    rooting.destroy();
+                }
+            }
+
+            return NO_DATA_FOUND;
         }
 
 }
