1cc81ccceb991f18036cc471556db58e93d4b747
Blechd0se <alex.christ@hotmail.de>
2016-04-03 15:09:54 +0200

    material: rippleview: fix one crash for android m
    Signed-off-by: Blechd0se <alex.christ@hotmail.de>

--- AeroControl/src/com/aero/control/helpers/Android/Material/RippleView.java
+++ AeroControl/src/com/aero/control/helpers/Android/Material/RippleView.java
@@ -9,6 +9,7 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.os.Build;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
@@ -126,7 +127,11 @@ public class RippleView extends RelativeLayout {
                 timer = 0;
                 durationEmpty = -1;
                 timerEmpty = 0;
-                canvas.restore();
+
+                if(Build.VERSION.SDK_INT != 23) {
+                    canvas.restore();
+                }
+
                 invalidate();
                 return;
             } else {
